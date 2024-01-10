dfe0ac3b2c509afc98d575540dc40a409249fd4f
Anurag Soni <anurag.soni@indiamart.com>
2024-01-10 11:12:56 +0530

    prop

---
+++ a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> summaryRanges(vector<int>& nums) {
+    list<int>
+}
+
+int main(){
+    return 0;
+}
---
+++ javascript/test.js
@@ -0,0 +1,15 @@
+console.log('HIIII')
+
+const obj = {
+    name: 'anurag',
+    show: function () {
+        return this
+    }
+};
+
+const obj2 = {
+    class: '12th',
+    __proto__: obj
+}
+
+console.log(obj2.show())
