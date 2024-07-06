07f299ebfa7ef431030462aab183dac2fbfe509d
Anurag Soni <anurag.soni@indiamart.com>
2024-07-06 22:24:38 +0530

    16. 3Sum Closest

--- lib/main.cpp
+++ lib/main.cpp
@@ -6,10 +6,38 @@ using ll = long long;
 class Solution
 {
 public:
+    int threeSumClosest(vector<int> &nums, int target)
+    {
+        sort(nums.begin(), nums.end());
+        int maxC = INT_MAX;
+        int ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (abs(sum - target) < maxC)
+                {
+                    maxC = abs(sum - target);
+                    ans = sum;
+                }
+                if (sum == target)
+                    return sum;
+                else if (sum < target)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return ans;
+    }
 };
 
 int main()
 {
+    vector<int> v = {-1, 2, 1, -4};
     Solution s = Solution();
+    cout << s.threeSumClosest(v, 1) << endl;
     return 0;
 }
