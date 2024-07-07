9ab62b42c9bec2448e3faf51b8130a4223824e1e
Anurag Soni <anurag.soni@indiamart.com>
2024-07-07 18:12:07 +0530

    22. Generate Parentheses

---
+++ _/22. Generate Parentheses.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include "utils/common.h"
+using namespace std;
+
+#define ll long long;
+
+class Solution
+{
+public:
+    vector<string> generateParenthesis(int n)
+    {
+        l = n;
+        backtrack(0, 0, "");
+        return ans;
+    }
+
+private:
+    vector<string> ans = {};
+    int l = 0;
+    void backtrack(int open, int close, string cur)
+    {
+        if (cur.size() == 2 * l)
+        {
+            cout << cur << endl;
+            ans.push_back(cur);
+            return;
+        }
+        if (open < l)
+            backtrack(open + 1, close, cur + "(");
+        if (close < open)
+            backtrack(open, close + 1, cur + ")");
+    }
+};
+
+int main()
+{
+    Solution s = Solution();
+    vector<string> t = s.generateParenthesis(3);
+    cout << endl;
+    return 0;
+}
